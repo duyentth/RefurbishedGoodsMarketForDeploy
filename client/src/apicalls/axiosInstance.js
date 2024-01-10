93b803730a587329a4292dcedfbc666ee0304ca1
duyentth <112973485+duyentth@users.noreply.github.com>
2024-01-10 17:25:43 -0500

    Update axiosInstance.js

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000";
+//axios.defaults.baseURL = "http://localhost:3000";
 export const axiosInstance = axios.create({
     headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
